542518914c5e24326248816779f6e30e32270f8e
Barabas <31799121+Barabazs@users.noreply.github.com>
2021-11-04 19:24:29 +0100

    test

--- metadata/pulling.py
+++ metadata/pulling.py
@@ -9,11 +9,13 @@ from web3 import Web3
 import ipfshttpclient
 import re
 
-ABI_ENDPOINT = 'https://api.etherscan.io/api?module=contract&action=getabi&address='
-ENDPOINT = ''
-ATTRIBUTES_FOLDER = 'raw_attributes'
-IMPLEMENTATION_SLOT = '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
-IPFS_GATEWAY = ''
+ABI_ENDPOINT = "https://api.etherscan.io/api?module=contract&action=getabi&address="
+ENDPOINT = ""
+ATTRIBUTES_FOLDER = "raw_attributes"
+IMPLEMENTATION_SLOT = (
+    "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc"
+)
+IPFS_GATEWAY = ""
 
 
 """ 
